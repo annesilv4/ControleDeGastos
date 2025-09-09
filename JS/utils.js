fbf58a043a63d218bb07cc03f272d8cee4039154
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-09-09 17:53:42 -0300

    Corrigindo erro de digitação na função atualizarInterface e adicionando formatação de moeda

---
+++ JS/utils.js
@@ -0,0 +1,17 @@
+// Funções utilitárias
+const obterElemento = (id) => document.getElementById(id);
+const formataMoeda = (valor) => valor.toFixed(2).replace(".", ",");
+
+// Funções Puras
+const isNegativo = (valor) => valor < 0;
+const atualizarInterface = (gastosPorCategoria) => {
+    const categorias = gastosPorCategoria.categorias;
+
+    categorias.forEaxh((categoria) => {
+        const element = obterElemento(categoria.nome);
+        element.textContent = `${categoria.nome}: R$ ${formataMoeda(categoria.valor)}`;
+    });
+
+    const elementTotal = obterElemento("Total");
+    elementTotal.textContent = `Total: R$ ${formataMoeda(gastosPorCategoria.obterTotal())}`;
+};
